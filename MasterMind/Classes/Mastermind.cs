831ca51a4befd82d1cda33b6a21f341406e40305
Amy Gurski <amygurski@gmail.com>
2020-02-16 10:34:57 -0500

    Fixes 10 try bug and seperates UI class

---
+++ MasterMind/Classes/Mastermind.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterMind.Classes
+{
+    public class Mastermind
+    {
+        #region Properties
+
+        public static int NumberOfTries = 10;
+
+        public static int[] Solution {
+            get
+            {
+                return GenerateSecretCode();
+            }
+        }
+        #endregion
+
+        #region Methods
+        public static void Play()
+        {
+            int[] guess;
+
+            while (NumberOfTries > 0) 
+            {
+                guess = UI.GetGuess();
+                NumberOfTries--;
+                int result = CheckGuess(guess);
+                UI.PrintWinLose(result, NumberOfTries);
+            }
+        }
+
+        public static int CheckGuess(int[] guess)
+        {
+            int fourToWin = 0;
+            string feedback = "";
+
+            //Copy solution to replace matched positions with 0's
+            int[] modifiedSolution = new int[4];
+            Array.Copy(Solution, modifiedSolution, Solution.Length);
+
+            for (int i = 0; i < guess.Length; i++) //For each number in guess
+            {
+
+                if (guess[i] == modifiedSolution[i])
+                {
+                    feedback = feedback.Insert(0, "+"); //+'s get printed first
+                    fourToWin++;
+                    modifiedSolution[i] = 0;
+                    continue;
+                }
+                else
+                {
+                    for (int j = 0; j < modifiedSolution.Length; j++) //For each number in solution
+                    {
+                        if (guess[i] == modifiedSolution[j] && guess[j] != modifiedSolution[j])
+                        {
+                            feedback += "-";
+                            modifiedSolution[j] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            UI.PrintFeedback(feedback);
+
+            return fourToWin;
+        }
+
+        private static int[] GenerateSecretCode()
+        {
+            int[] fourRandomNumbers = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                fourRandomNumbers[i] = RandomNumber(1, 7);
+            }
+
+            return fourRandomNumbers;
+        }
+
+        static private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        #endregion
+    }
+}
--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MasterMind.Classes;
 
 namespace MasterMind
 {
@@ -6,76 +7,28 @@ namespace MasterMind
     {
         static void Main(string[] args)
         {
-            int[] fourRandomNumbers = new int[4]; //The number to be guessed
-            int[] guessArray = new int[4]; //guesses stored as array
-            int fullGuess = 0;
-            int numberOfGuessesRemaining = 10;
 
-            PrintInstructions();
-                        
-            //Generate 4 random numbers between 1 and 6 inclusive for the player to guess
-            for (int i = 0; i < fourRandomNumbers.Length; i++)
-            {
-                fourRandomNumbers[i] = RandomNumber(1, 7);
-                Console.Write(fourRandomNumbers[i]); //Just for debugging
-            }
+            /*
+             * Create a C# console application that is a simple version of Mastermind. 
+             * Criteria: 
+             * 1. The randomly generated solution should be four (4) digits in length, with each digit ranging from 1 to 6.
+             * 2. After the player enters a combination, a minus (-) sign should be printed for every digit that is correct but in the wrong position, 
+             *    and a plus (+) sign should be printed for every digit that is both correct and in the correct position. 
+             * 3. Print all plus signs first, all minus signs second, and nothing for incorrect digits. 
+             * 4. The player has ten (10) attempts to guess the number correctly before receiving a message that they have lost.
+            */
 
-            // Have a counter for 10 guesses, working out 1 guess first
-            //while (numberOfGuessesRemaining > 0)
-            //{
-                string playerInput = PlayerGuess();
+            UI.Title();
 
-                //Make sure player input meets criteria (4 integers between 1 and 6)
+            //Case 1: View Instructions
+            //Case 2: Play Mastermind
+            //Case 3: Quit
+            //Case 4: View stats (x plays, 
 
-                //makes sure it's an int. I don't like that I have to assign an output since not using
-                while (!int.TryParse(playerInput, out fullGuess))
-                {
-                    Console.WriteLine("This is not a number.");
-                    playerInput = PlayerGuess();
-                }
-
-                //if it's not a 4 digit int, error
-                while (playerInput.Length != 4)
-                {
-                    Console.WriteLine("Please enter 4 digits.");
-                    playerInput = PlayerGuess();
-                }
-
-            //Convert to an int array but if any digit isn't between 1 and 6, reprompt.
-            //for (int i = 0; i < guessArray.Length; i++)
-            //{
-            //    guessArray[i] = int.Parse(playerInput[i]);
-            //}
-
-            numberOfGuessesRemaining--;
-            //}
-
-            //Print outcome:
-            //if identical, win
-            //Check if each digit exists in array and at same position = +
-            //Exists in array but at different position = -
-            //Blank if neither
-
-            //If wrong after 10 tries, you lose.
+            //Mastermind mm = new Mastermind();
+            Mastermind.Play();
         }
 
-        static public int RandomNumber(int min, int max)
-        {
-            Random random = new Random();
-            return random.Next(min, max);
-        }
-
-        static public void PrintInstructions()
-        {
-            Console.WriteLine("\nMasterMind Instructions:\nI'm thinking of a 4 digit number.\nEach number is between 1 and 6.\nYou will have 10 tries to solve it.\n");
-            Console.WriteLine("(-) indicates that a digit is correct, but in the wrong position.\n(+) indicates that both the digit and position are correct.\n");
-
-        }
-
-        static public string PlayerGuess()
-        {
-            Console.WriteLine("\nWhat's your guess?");
-            return Console.ReadLine();
-        }
+        
     }
 }
